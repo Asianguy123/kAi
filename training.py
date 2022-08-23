7d29a64960500095137958b2856a28e2a002cc7f
Asianguy123 <sas@drsyed.org>
2022-08-23 20:53:01 +0530

    Update training.py

--- training.py
+++ training.py
@@ -18,3 +18,31 @@ from nltk.stem import WordNetLemmatizer
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import SGD
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Other Functions
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Main functions
+
+def training_main():
+    pass
+
+def call_tm():
+    pass
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Globals
+
+current_path = os.getcwd()
+corpora = [file for file in os.listdir(f'{current_path}/corpora/') if file.endswith('.json')]
+lemmatiser = WordNetLemmatizer()
+ignore_chrs = ['?', '!', '.', ',', "'", '"', '/', '£', '$', 
+                '%', '^', '&', '*', '@', ':', ';', '#', '~', 
+                '|', '<', '>', '{', '}', '_', '-', '+', '='
+]
+
+
+
+
