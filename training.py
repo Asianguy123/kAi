dce07a0bbc952514ccfaaaf1d5c8650c765bdeb9
Asianguy123 <sas@drsyed.org>
2022-08-23 20:52:47 +0530

    Update training.py

--- training.py
+++ training.py
@@ -4,3 +4,17 @@
 Creates the models based on the corpora
 Runs only if models don't exist
 '''
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Imports
+
+import os
+import random
+import pickle
+import random
+import numpy
+import nltk
+from nltk.stem import WordNetLemmatizer
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import SGD
