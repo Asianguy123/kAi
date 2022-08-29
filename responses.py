e01f7762894cef135e6315830af5a097fc64694f
Asianguy123 <sas@drsyed.org>
2022-08-29 20:51:13 +0530

    Update responses.py

--- responses.py
+++ responses.py
@@ -5,3 +5,15 @@ Takes input from cwi.py and passes into model
 Best response is gained
 Response outputted to cwi.py for display
 '''
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Imports
+
+import os
+import pickle
+import random
+import json
+import numpy
+import nltk
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
