7dba6cd25ad9c7d0f9dde168ea61f0c0d6e03b99
Asianguy123 <sas@drsyed.org>
2022-11-09 11:30:02 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -32,3 +32,15 @@ def main():
         pygame.display.flip()
         CLOCK.tick(60)
         SCREEN.blit(bg_image, (0,0))
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Setup
+
+pygame.init()
+CLOCK = pygame.time.Clock()
+SCREEN_WIDTH = 571
+SCREEN_HEIGHT = 720 
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.NOFRAME)
+pygame.display.set_caption('kAi')
+current_dir = os.getcwd()
+bg_image = pygame.image.load(f'{current_dir}/Images/main_menu.tif')
