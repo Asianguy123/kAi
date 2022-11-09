b997f03e9acd907b0c66966b4811549408b38718
Asianguy123 <sas@drsyed.org>
2022-11-09 11:28:36 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -11,3 +11,24 @@ Menu interface - has sound options + advance to chat session
 import os
 import sys
 import pygame
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Main Function
+
+def main():
+    click = False
+    while True:
+        
+        mx, my = pygame.mouse.get_pos() 
+       
+        # event loop
+        click = False
+        for event in pygame.event.get():    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+                    
+        # window update
+        pygame.display.flip()
+        CLOCK.tick(60)
+        SCREEN.blit(bg_image, (0,0))
