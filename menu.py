c5390048ff96fdd1eb3cf3794581fd2735f6457c
Asianguy123 <sas@drsyed.org>
2022-11-28 10:16:32 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -18,6 +18,18 @@ import win32gui
 import cwi
 
 # ---------------------------------------------------------------------------------------------------------------------
+# Support Functions
+
+def draw_rect_transparent(surface, colour, rect):
+    '''
+    Draws transparent rectangles on given surface
+    '''
+
+    shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA) # makes a surface from rectangles of given rectangles, with alpha property
+    pygame.draw.rect(shape_surf, colour, shape_surf.get_rect()) # draws rectangle using surface
+    surface.blit(shape_surf, rect)
+
+# ---------------------------------------------------------------------------------------------------------------------
 # Main Function
 
 def main():
