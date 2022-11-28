fef743747d5b3946021e311bd8ad7b230b8f761f
Asianguy123 <sas@drsyed.org>
2022-11-28 10:17:07 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -29,6 +29,16 @@ def draw_rect_transparent(surface, colour, rect):
     pygame.draw.rect(shape_surf, colour, shape_surf.get_rect()) # draws rectangle using surface
     surface.blit(shape_surf, rect)
 
+def draw_centred_text(text, font, color, surface, x, y):
+    '''
+    Outputs text at the specified location, with (x,y) being the centre of the text object, using the required parameters
+    '''
+
+    text_obj = font.render(text, 1, color)
+    text_rect = text_obj.get_rect()
+    text_rect.center = (x, y)
+    surface.blit(text_obj, text_rect)
+    
 # ---------------------------------------------------------------------------------------------------------------------
 # Main Function
 
