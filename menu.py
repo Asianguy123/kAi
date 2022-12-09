2e50dc5f301d869520ac47fd05fd569c7a401044
Asianguy123 <sas@drsyed.org>
2022-12-09 10:50:55 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -136,3 +136,28 @@ def main():
         CLOCK.tick(60)
         SCREEN.fill(transparent)
         SCREEN.blit(bg_image, (354,0))
+    
+# ---------------------------------------------------------------------------------------------------------------------
+# Setup
+
+pygame.init()
+CLOCK = pygame.time.Clock()
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.NOFRAME)
+current_dir = os.getcwd()
+bg_image = pygame.image.load(f'{current_dir}/images/main_menu.tif')
+font_s = pygame.font.SysFont('Calibri', 20, bold=True)
+font_r = pygame.font.SysFont('Calibri', 18, bold=True)
+
+# making the empty background transparent
+transparent = (255, 0, 0)
+hwnd = pygame.display.get_wm_info()["window"]
+win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*transparent), 0, win32con.LWA_COLORKEY)
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Runs Code
+
+if __name__ == '__main__':
+    main()
