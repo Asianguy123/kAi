d7b3da63bb0d9494aaa376394a596e7892a555cc
Asianguy123 <sas@drsyed.org>
2022-12-11 18:33:42 +0000

    Update user_manual.py

--- user_manual.py
+++ user_manual.py
@@ -14,3 +14,15 @@ import pygame
 import menu
 from menu import SCREEN
 from menu import CLOCK
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Utility Functions
+
+def draw_rect_transparent(surface, colour, rect):
+    '''
+    Draws transparent rectangles on given surface
+    '''
+
+    shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA) # makes a surface from rectangles of given rectangles, with alpha property
+    pygame.draw.rect(shape_surf, colour, shape_surf.get_rect()) # draws rectangle using surface
+    surface.blit(shape_surf, rect)
