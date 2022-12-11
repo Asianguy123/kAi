ca9dd32f6cff0596052eff966227b50ac5da82a7
Asianguy123 <sas@drsyed.org>
2022-12-11 18:33:21 +0000

    Create user_manual.py

---
+++ user_manual.py
@@ -0,0 +1,16 @@
+# User Manual
+
+'''
+Provides a slideshow display of how to use the application
+Can be access from the main menu, and the chat window
+'''
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Imports
+
+import os
+import sys
+import pygame
+import menu
+from menu import SCREEN
+from menu import CLOCK
