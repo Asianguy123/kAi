cc833dde3cff9026bced58b413e6f17797f70b27
Asianguy123 <sas@drsyed.org>
2022-12-11 18:37:47 +0000

    Update cwi.py

--- cwi.py
+++ cwi.py
@@ -5,3 +5,11 @@ The Main User Interface, where the chat session occurs
 Output responses.py is fed into this file and outputted
 '''
 
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Imports
+
+import os
+import sys
+import pygame
+import menu
+import user_manual
