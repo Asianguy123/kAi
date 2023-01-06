a7949866232ebd57eabe55541062bd3bbf9c223d
Asianguy123 <sas@drsyed.org>
2023-01-06 10:41:29 +0000

    Create graphics.py

---
+++ graphics.py
@@ -0,0 +1,8 @@
+# Graphical User Interface for kAi
+
+'''
+Main file ran on application start
+
+Contains all graphical user interfaces: menu, user manual and chat window
+Imports AI responses from responses.py, which is trained using training.py
+'''
