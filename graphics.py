2f33c07f9abc3ea6d62ddf73b10a78016a8e1c40
Asianguy123 <sas@drsyed.org>
2023-03-19 22:27:42 +0000

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -337,3 +337,8 @@ def chat_window(notif_s, notif_r):
                     if notif_s:
                         pygame.mixer.Sound.play(MESSAGE_S_NOTIF)
                     text = ''
+            
+            # message input box click detection
+            if message_box.collidepoint(mx, my):
+                typing_active = True
+                pretext = ''
